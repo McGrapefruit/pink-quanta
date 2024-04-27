8d04b4f8076aeb374204c277f44f0444a1843672
Nikolaus Leonard <Grapefruit@Grapefruit.fritz.box>
2024-04-27 02:59:17 +0200

    Quartz sync: Apr 27, 2024, 2:59 AM

--- quartz/components/SiteLogo.tsx
+++ quartz/components/SiteLogo.tsx
@@ -1,7 +1,7 @@
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 function SiteLogo({ cfg, fileData }: QuartzComponentProps) {
-  const ogImagePath = `./quartz/static/pink_quanta_white_300.png`
+  const ogImagePath = `https://mcgrapefruit.github.io/pink-quanta/quartz/static/pink_quanta_white_300.png`
   return <div><a href="https://mcgrapefruit.github.io/pink-quanta/"><img class="site-logo" src={ogImagePath} alt="Return to Home Page"></img></a></div>
 }
 SiteLogo.css = `
